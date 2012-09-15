e3758a3f61d7c21531393e79b34d9aaa0d416842
Joe LaBarbera <jlabarbera11@gmail.com>
2012-09-15 12:22:52 -0400

    added junit

--- Bank/src/messaging/Messaging.java
+++ Bank/src/messaging/Messaging.java
@@ -1,21 +1,37 @@
 package messaging;
 
+import java.util.Scanner;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+
 
 public class Messaging {
 
     private String account;
-    private Map topology;
+    private Map<String, Set<String>> topology;
 
     public void Messaging(String new_acnt) {
         account = new_acnt;
 
-        topology = new HashMap();
-
+        topology = new HashMap<String, Set<String>>();
+        Scanner scanner = new Scanner("topology.txt");
+        while (scanner.hasNextLine()) {
+            String[] a = scanner.nextLine().split(" ");
+            if (topology.containsKey(a[0])) {
+                topology.get(a[0]).add(a[1]);
+            }
+            else {
+                Set<String> s = new HashSet<String>();
+                s.add(a[1]);
+                topology.put(a[0], s);
+            }
+        }
     }
 
-    public void Deposit(String acnt, float amt, float ser_number) {}
+    public void Deposit(String acnt, float amt, float ser_number) {
+    }
 
 
 }
---
+++ Bank/src/messaging/test/MessagingTest.java
@@ -0,0 +1,9 @@
+package messaging.test;
+
+import org.junit.*;
+
+public class MessagingTest {
+
+    @Test public void testTopology() throws Exception {
+    }
+}
