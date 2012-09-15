96110a2417631c974246d633746cf0ac84c595e9
Joe LaBarbera <jlabarbera11@gmail.com>
2012-09-15 10:47:03 -0400

    added server and messaging packages

---
+++ Bank/src/messaging/Messaging.java
@@ -0,0 +1,14 @@
+package messaging;
+
+public class Messaging {
+
+    private String account;
+
+    public void Messaging(Sting new_acnt) {
+        account = new_acnt;
+    }
+
+    public void Deposit(String acnt, float amt, float ser_number) {
+
+
+}
