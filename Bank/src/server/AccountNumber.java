e7095fb92b3aa258cb9676a56ea4f37cf7be32f6
Kyle Hsu <kkh48@cornell.edu>
2012-09-16 17:03:40 -0400

    Account Number change

---
+++ Bank/src/server/AccountNumber.java
@@ -0,0 +1,40 @@
+package server;
+
+public class AccountNumber
+{
+	private int branch;
+	private int account;
+
+	public AccountNumber(int branch, int account)
+	{
+		assert branch >= 0 && branch <= 99 : "branch: " + branch;
+		assert account >= 0 && account <= 99999 : "account: " + account;
+
+		this.branch = branch;
+		this.account = account;
+	}
+
+	@Override
+	public String toString()
+	{
+		return String.format("%02d.%05d", branch, account);
+	}
+
+	@Override
+	public boolean equals(Object that)
+	{
+		if (this == that)
+			return true;
+
+		if (!(that instanceof AccountNumber))
+			return false;
+
+		AccountNumber an = (AccountNumber) that;
+		return branch == an.branch && account == an.account;
+	}
+
+	@Override
+	public int hashCode() {
+		return branch * 100000 + account;
+	}
+}
