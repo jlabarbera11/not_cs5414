7f4409848db1e887a7691e481136fafc2520a3fe
Joe LaBarbera <jlabarbera11@gmail.com>
2012-09-16 18:58:54 -0400

    added Launch script for 4 branches

--- Bank/src/messaging/test/MessagingTest.java
+++ Bank/src/messaging/test/MessagingTest.java
@@ -1,11 +1,9 @@
 package messaging.test;
 
 import org.junit.*;
-import 
 
 public class MessagingTest {
 
     @Test public void testTopology() throws Exception {
-	Messaging m = new Messaging(
     }
 }
