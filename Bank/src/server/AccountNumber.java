49cdcb182b2fdc6ae12f7a2fa9ac5f4c050474a5
Joe LaBarbera <jlabarbera11@gmail.com>
2012-09-16 19:45:43 -0400

    changed Branch to Server

--- Bank/src/server/AccountNumber.java
+++ Bank/src/server/AccountNumber.java
@@ -2,44 +2,44 @@ package server;
 
 public class AccountNumber
 {
-	private int branch;
-	private int account;
+    private int branch;
+    private int account;
 
-	public AccountNumber(int branch, int account)
-	{
-		assert branch >= 0 && branch <= 99 : "branch: " + branch;
-		assert account >= 0 && account <= 99999 : "account: " + account;
+    public AccountNumber(int branch, int account)
+    {
+        assert branch >= 0 && branch <= 99 : "branch: " + branch;
+        assert account >= 0 && account <= 99999 : "account: " + account;
 
-		this.branch = branch;
-		this.account = account;
-	}
+        this.branch = branch;
+        this.account = account;
+    }
 
-	public int getBranch()
-	{
-		return branch;
-	}
+    public int getBranch()
+    {
+        return branch;
+    }
 
-	@Override
-	public String toString()
-	{
-		return String.format("%02d.%05d", branch, account);
-	}
+    @Override
+    public String toString()
+    {
+        return String.format("%02d.%05d", branch, account);
+    }
 
-	@Override
-	public boolean equals(Object that)
-	{
-		if (this == that)
-			return true;
+    @Override
+    public boolean equals(Object that)
+    {
+        if (this == that)
+            return true;
 
-		if (!(that instanceof AccountNumber))
-			return false;
+        if (!(that instanceof AccountNumber))
+            return false;
 
-		AccountNumber an = (AccountNumber) that;
-		return branch == an.branch && account == an.account;
-	}
+        AccountNumber an = (AccountNumber) that;
+        return branch == an.branch && account == an.account;
+    }
 
-	@Override
-	public int hashCode() {
-		return branch * 100000 + account;
-	}
+    @Override
+    public int hashCode() {
+        return branch * 100000 + account;
+    }
 }
