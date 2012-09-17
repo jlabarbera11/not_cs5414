d0e47a8c5c126482902fb950ddfc4f9181a950c1
Joe LaBarbera <jlabarbera11@gmail.com>
2012-09-16 21:07:17 -0400

    added branch info to transfer packet

--- Bank/src/messaging/TransferRequest.java
+++ Bank/src/messaging/TransferRequest.java
@@ -2,14 +2,20 @@ package messaging;
 
 public class TransferRequest extends MessageRequest {
 
+    private Integer dest_branch
     private Integer dest_acnt;
     private Float amt;
 
-    public TransferRequest(Integer src_acnt, Integer dest_acnt, Float amt, Integer ser_number) {
+    public TransferRequest(Integer dest_branch, Integer src_acnt, Integer dest_acnt, Float amt, Integer ser_number) {
+        this.dest_branch = dest_branch;
         this.acnt = src_acnt;
         this.dest_acnt = dest_acnt;
         this.amt = amt;
         this.ser_number = ser_number;
+    }
+
+    public Integer getDestBranch() {
+        return this.dest_branch;
     }
 
     public Integer getSrcAcnt() {
