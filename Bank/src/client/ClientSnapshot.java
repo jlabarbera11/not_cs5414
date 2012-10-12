136e561972f9e2a22dca833d8be43210960480d1
Ben Schoener <bschoener@gmail.com>
2012-10-11 22:29:12 -0700

    some stuff

--- Bank/src/client/ClientSnapshot.java
+++ Bank/src/client/ClientSnapshot.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -37,34 +38,52 @@ public class ClientSnapshot implements Callback {
 	    Snapshot snapshot = response.getSnapshot();
 	    JFrame newFrame = new JFrame();
 	    String[][] rows = new String[snapshot.branchState.size()][2];
+	    System.out.println("branch state size is: " + snapshot.branchState.size());
 	    String[] column_names = {"Account Number", "Balance:"};
 	    Iterator<BankAccount> iterator = snapshot.branchState.iterator();
 	    int i=0;
 	    while(iterator.hasNext()){
 	    	BankAccount account = iterator.next();
-	    	rows[i][0]=""+account.getAccountNumber();
-	    	rows[i][1]=""+account.getBalance();
+	    	rows[i][0]="asdf"+account.getAccountNumber();
+	    	rows[i][1]="asdf"+account.getBalance();
 	    	i++;
 	    }
 	    JTable table = new JTable(rows, column_names);
 	    JScrollPane scrollPane = new JScrollPane(table);
 	    newFrame.getContentPane().add(scrollPane, BorderLayout.CENTER);
-	    
-	    
-	    //private Map<Integer, Channel> channels;
-	    JPanel panel = new JPanel();
-	    for (Map.Entry<Integer, Channel> entry : snapshot.channels.entrySet()) {
-	    	panel.add(new JLabel("Messages in channel from branch " + entry.getKey()));
-	    	panel.add(new JLabel(entry.getValue().toString()));
-	    }
-	    newFrame.getContentPane().add(panel, BorderLayout.CENTER);
-	    
-	    
 	  
 	    newFrame.pack();
 	    newFrame.setVisible(true);
 	    
+	    makeAnotherWindow(snapshot);
     }
+
+	private static void makeAnotherWindow() {
+		
+		JFrame newFrame = new JFrame();
+		
+	    //private Map<Integer, Channel> channels;
+	    JPanel panel = new JPanel();
+	    for (Map.Entry<Integer, Channel> entry : snapshot.channels.entrySet()) {
+	    	//panel.add(new JLabel("Messages in channel from branch " + entry.getKey()));
+	    	panel.add(new JLabel(entry.getValue().toString()));
+	    	
+	    	newFrame.getContentPane().add(new JLabel("Messages in channel from branch " + entry.getKey()), BorderLayout.CENTER);
+	    	Channel channel = entry.getValue();
+	    }
+		
+	    
+		  
+	    newFrame.pack();
+	    newFrame.setVisible(true);
+		
+	}
+    
+	public static void main(String[] args){
+		makeAnotherWindow();
+		
+	}
+    
 }
 
 
