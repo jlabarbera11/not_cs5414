b4b6f00371aac1dd36f0a821ec77d5746127ac41
Ben Schoener <bschoener@gmail.com>
2012-11-08 22:28:01 -0500

    asdf

--- LAUNCH.cmd
+++ LAUNCH.cmd
@@ -1,14 +1,10 @@
 cd Bank/
 
-start java -jar server.jar 01
+start java -jar server.jar 01 01
+start java -jar server.jar 01 02
+start java -jar server.jar 01 03
+start java -jar server.jar 02 01
+start java -jar oracle.jar
 start java -jar client.jar 01
-start 
-start java -jar server.jar 02
 start java -jar client.jar 02
-start 
-start java -jar server.jar 03
-start java -jar client.jar 03
-start 
-start java -jar server.jar 04
-start java -jar client.jar 04
 
