61c47c803c5b90e3d30d179b03e37996dcd2d152
Joe LaBarbera <jlabarbera11@gmail.com>
2012-12-03 01:18:39 -0500

    more check

--- Bank/src/jvm/FailureDetector.java
+++ Bank/src/jvm/FailureDetector.java
@@ -9,6 +9,7 @@ import oracle.Oracle;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Set;
 
 import java.net.Socket;
 import java.net.ServerSocket;
@@ -30,17 +31,17 @@ public class FailureDetector extends Thread {
 
   public FailureDetector(Integer fid, Integer port) {
     this.fid = fid;
-
-    for(Integer i: readjvmInfo.keySet()) {
-      Map<ReplicaID, long> lm = new HashMap<ReplicaID, long>();
-      for(ReplicaID rid : rids)
-        lm.put(rid, 0);
-      rts.put(i, lm);
-      fts.put(i, 0);
-    }
-
     this.rts = new HashMap<Integer, Map<ReplicaID, Long>>();
     this.fts = new HashMap<Integer, Long>();
+
+    for(Map.Entry<Integer, Set<ReplicaID>> entry : JVM.readjvmInfo().entrySet()) {
+      Map<ReplicaID, Long> lm = new HashMap<ReplicaID, Long>();
+      for(ReplicaID rid : entry.getValue())
+        lm.put(rid, new Long(0));
+      this.rts.put(entry.getKey(), lm);
+      this.fts.put(entry.getKey(), new Long(0));
+    }
+
     try {
       this.ss = new ServerSocket(port);
       ss.setReuseAddress(true);
